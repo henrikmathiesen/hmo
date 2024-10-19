8554301fe575b49a1258dbf8919d4396ce6f04d5
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-10-19 19:35:15 +0200

    Begun Om

--- src/app/routes/om/om.component.ts
+++ src/app/routes/om/om.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components';
 
 @Component({
     selector: 'app-om',
     standalone: true,
+    imports: [CardComponent],
     templateUrl: './om.component.html'
 })
 export class OmComponent { }
