22f3fa14a808e59ad988e5ce5c926237fb060756
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-10-25 21:23:21 +0200

    Text

--- src/app/routes/terapin/terapin.component.ts
+++ src/app/routes/terapin/terapin.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components';
 
 @Component({
     selector: 'app-terapin',
     standalone: true,
+    imports: [CardComponent],
     templateUrl: './terapin.component.html'
 })
 export class TerapinComponent { }
