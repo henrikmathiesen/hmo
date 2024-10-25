423aa407b20f6cdb63899ededdd45f32b9fd5fff
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-10-25 19:50:00 +0200

    Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from './components';
+import { NgClass } from '@angular/common';
 
+import { NavComponent } from './components';
 import { FeatherIcon } from './directives';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FeatherIcon],
+  imports: [RouterOutlet, NavComponent, FeatherIcon, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Hjälp med OCD!';
+  isStandAloneForm = false;
+
+  onActivate(event: any) {
+    this.isStandAloneForm = !!event.testForm;
+  }
 }
