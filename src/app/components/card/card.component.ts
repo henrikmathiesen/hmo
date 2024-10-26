7b57ce5589ad73d9e3f319fdcc6c37c313110416
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-10-26 17:45:25 +0200

    Card supports icon

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+
+import { FeatherIcon } from '../../directives';
 
 @Component({
     selector: 'app-card',
     standalone: true,
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
-    imports: [NgClass]
+    imports: [NgClass, NgIf, FeatherIcon]
 })
 export class CardComponent {
     @Input() header = '';
+    @Input() iconType: 'user' | 'book' | '' = '';
 }
