32a05159b6733bc9e64898a00aa9958bc2c1f2df
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-10-26 17:46:16 +0200

    Resurser design, placeholders. Todo comments.

--- src/app/routes/resurser/resurser.component.ts
+++ src/app/routes/resurser/resurser.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent, QuoteComponent } from '../../components';
 
 @Component({
     selector: 'app-resurser',
     standalone: true,
+    imports: [CardComponent, QuoteComponent],
     templateUrl: './resurser.component.html'
 })
 export class ResurserComponent { }
