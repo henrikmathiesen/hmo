2c1b4a60a1a0a09ba2e3ab722987f22abfbb229d
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-11-02 11:33:26 +0100

    Design. Text.

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-quote',
     standalone: true,
-    templateUrl: './quote.component.html'
+    imports: [NgClass],
+    templateUrl: './quote.component.html',
+    styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent {
     @Input() p = '';
     @Input() f = '';
+    @Input() smallP = false;
 }
