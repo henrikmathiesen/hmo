20e2396cf211087ec7a36074d559b2558401493e
henrikmathiesen <henrik_mathiesen@hotmail.com>
2025-01-19 14:43:00 +0100

    Swedish culture

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeSe from '@angular/common/locales/sv';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localeSe);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()),
+    { provide: LOCALE_ID, useValue: 'sv-SE' }
+  ]
 };
--- src/app/routes/resurser/tracker/tracker.component.ts
+++ src/app/routes/resurser/tracker/tracker.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-tracker',
     standalone: true,
-    imports: [],
+    imports: [DatePipe],
     templateUrl: './tracker.component.html'
 })
-export class TrackerComponent { }
+export class TrackerComponent { 
+    d = new Date('2025-05-02');
+}
