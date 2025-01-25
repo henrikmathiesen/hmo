fa3bfcc0dac6c0287a67e3aaf8bc2d4cb34bcc07
henrikmathiesen <henrik_mathiesen@hotmail.com>
2025-01-25 15:30:24 +0100

    Save to localstorage, render answer

--- src/app/models/tracker.interface.ts
+++ src/app/models/tracker.interface.ts
@@ -1,8 +1,6 @@
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { RatingEnum } from './rating.enum';
 
 export interface TrackerInterface {
     id: string;
-    trackerCalendar: NgbDateStruct;
     rating: RatingEnum;
 }
--- src/app/routes/resurser/tracker/tracker.component.ts
+++ src/app/routes/resurser/tracker/tracker.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbCalendar, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
@@ -8,15 +8,17 @@ import { RatingEnum, TrackerInterface } from '../../../models';
 
 @Component({
     selector: 'app-tracker',
-    imports: [DatePipe, FormsModule, NgbDatepickerModule, NgbDateToDatePipe],
+    imports: [NgIf, FormsModule, NgbDatepickerModule, DatePipe, NgbDateToDatePipe],
     templateUrl: './tracker.component.html'
 })
 export class TrackerComponent implements OnInit {
     trackerCalendarModel: NgbDateStruct = { day: 0, month: 0, year: 0 };
     ratingModel: RatingEnum = RatingEnum.placeholder;
     trackerModel: TrackerInterface[] = [];
-
     RatingEnum = RatingEnum;
+
+    ratingForSelectedDay: RatingEnum = RatingEnum.placeholder;
+    private readonly localStorageKey = 'hmo';
 
     constructor(
         private calendar: NgbCalendar
@@ -24,35 +26,37 @@ export class TrackerComponent implements OnInit {
 
     ngOnInit(): void {
         this.trackerCalendarModel = this.calendar.getToday();
+
+        let trackersInLocalStorage = localStorage.getItem(this.localStorageKey);
+        
+        if (trackersInLocalStorage) {
+            this.trackerModel = JSON.parse(trackersInLocalStorage);
+        }
+
+        this.updateRatingForSelectedDay();
     }
 
     onTrackerCalendarSelect(event: NgbDateStruct) {
         this.trackerCalendarModel = event;
         this.ratingModel = RatingEnum.placeholder;
+        this.updateRatingForSelectedDay();
     }
 
     onTrackerRatingSelect(event: string) {
-        console.log(event);
-
         if (!event || event === RatingEnum.placeholder) {
             return;
         }
 
         this.ratingModel = (event as RatingEnum);
 
-        console.log(
-            this.trackerCalendarModel,
-            this.ratingModel
-        );
-
         this.setInTracker();
+        this.updateRatingForSelectedDay();
     }
 
     private setInTracker() {
         const trackerItem: TrackerInterface = {
-            id: `${this.trackerCalendarModel?.year}_${this.trackerCalendarModel?.month}_${this.trackerCalendarModel?.day}`,
-            rating: this.ratingModel,
-            trackerCalendar: this.trackerCalendarModel
+            id: this.getId(),
+            rating: this.ratingModel
         };
 
         if (this.trackerModel.find(v => v.id === trackerItem.id)) {
@@ -64,6 +68,21 @@ export class TrackerComponent implements OnInit {
             this.trackerModel.push(trackerItem);
         }
 
-        console.log(this.trackerModel);
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.trackerModel));
+    }
+
+    private getId() {
+        return `${this.trackerCalendarModel.year}_${this.trackerCalendarModel.month}_${this.trackerCalendarModel.day}`;
+    }
+
+    private updateRatingForSelectedDay() {
+        const idForSelectedDay = this.getId();
+        const item = this.trackerModel.filter(v => v.id === idForSelectedDay)[0];
+
+        if (item) {
+            this.ratingForSelectedDay = item.rating;
+        } else {
+            this.ratingForSelectedDay = RatingEnum.placeholder;
+        }
     }
 }
