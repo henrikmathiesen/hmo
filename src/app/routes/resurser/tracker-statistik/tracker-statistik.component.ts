0c9215c3cbcf316fd92e7a44ecc41c7b9718656a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2025-02-23 00:08:28 +0100

    Render statistik view

--- src/app/routes/resurser/tracker-statistik/tracker-statistik.component.ts
+++ src/app/routes/resurser/tracker-statistik/tracker-statistik.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
-import { BackLinkComponent } from '../../../components';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { BackLinkComponent, QuoteComponent, RatingBadgeComponent } from '../../../components';
+import { LocalstorageKeysEnum, TrackerInterface, RatingEnum } from '../../../models';
+import { TrackerStatistikQuotePipe, TrackerStatistikAntalDagarPipe, TrackerStatistikProcentDagarPipe, DisplayPercentPipe } from '../../../pipes';
 
 @Component({
     selector: 'app-tracker-statistik',
-    imports: [BackLinkComponent],
+    imports: [
+        BackLinkComponent, 
+        QuoteComponent, 
+        RatingBadgeComponent, 
+        TrackerStatistikQuotePipe, 
+        TrackerStatistikAntalDagarPipe, 
+        TrackerStatistikProcentDagarPipe,
+        DisplayPercentPipe,
+        NgFor
+    ],
     templateUrl: './tracker-statistik.component.html'
 })
-export class TrackerStatistikComponent {}
+export class TrackerStatistikComponent implements OnInit {
+    private readonly localStorageKey = LocalstorageKeysEnum.tracker;
+    trackerModel: TrackerInterface[] = [];
+    ratingArr: string[] = [];
+
+    ngOnInit(): void {
+        let trackersInLocalStorage = localStorage.getItem(this.localStorageKey);
+
+        if (trackersInLocalStorage) {
+            this.trackerModel = JSON.parse(trackersInLocalStorage);
+        }
+
+        this.getRatingOptionsAsArr();
+    }
+
+    private getRatingOptionsAsArr() {
+        Object.keys(RatingEnum).forEach((key) => {
+            if (key !== 'placeholder') {
+                this.ratingArr.push((RatingEnum as any)[key]);
+            }
+        });
+    }
+
+}
